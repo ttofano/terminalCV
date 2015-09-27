b0c624c2ffd72847d2e61ad7152d207de6f8e208
Daniel Hauck <daniel@hauck.it>
2015-09-27 10:49:01 +0200

    hello github! ;)

---
+++ render_shell.py
@@ -0,0 +1,21 @@
+import yaml
+from sys import exit
+from jinja2 import Template
+from htmlmin import minify
+
+try:
+    about = yaml.load(open('about.yml', 'r').read())
+except IOError:
+    print "You need to provide a valid about.yml file!"
+    exit(1)
+
+if about['long_description'].endswith('\n'):
+    about['long_description'] = about['long_description'][:-2]
+
+for project in about['projects']:
+    if project['description'].endswith('\n'):
+        project['description'] = project['description'][:-2]
+
+template = Template(open('index.j2').read())
+open('www/index.html', 'w').write(template.render(about=about))
+
